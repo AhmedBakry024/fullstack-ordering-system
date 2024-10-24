a15d7a10760c496dcf8085d0052ff53afa332d49
Ahmed Bakry <ahmedbakry024@gmail.com>
2024-10-25 01:51:02 +0300

    Correct DB Schema

--- ordering-db.sql
+++ ordering-db.sql
@@ -1,3 +1,5 @@
+-- Active: 1729277811648@@127.0.0.1@3306@OrderingSystem
+
 CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -19,7 +21,7 @@ CREATE TABLE `orders` (
   `package_details` text NOT NULL,
   `delivery_time` date DEFAULT (CURRENT_DATE + INTERVAL 2 DAY) NOT NULL,
   `status` varchar(20) DEFAULT 'pending' COMMENT 'Can be pending, accepted, picked_up, in_transit, delivered, cancelled',
-  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP),
+  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
 );
 
 ALTER TABLE `orders` ADD FOREIGN KEY (`courier_id`) REFERENCES `users` (`id`);
