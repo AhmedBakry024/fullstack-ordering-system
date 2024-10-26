2ad82d76f426ecd4ea074b9546db22feac6842e0
Ahmed Hagag <100851728+hagag1919@users.noreply.github.com>
2024-10-26 11:46:35 +0300

    fix incorrect port

--- frontend/src/services/apiService.jsx
+++ frontend/src/services/apiService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8000"; // backend URL
+const API_URL = "http://localhost:8080"; // backend URL
 
 export const registerUser = async (userData) => {
     try {
