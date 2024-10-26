e202f14fee38eaff9de42e475f9785138e98c5b4
Ahmed Hagag <100851728+hagag1919@users.noreply.github.com>
2024-10-26 10:58:33 +0300

    create a new project for frontend and fix run bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3030, // Ensure the port is correctly set
-    open: true, // Automatically open the browser on server start
-  },
-  resolve: {
-    alias: {
-      '@': '/src', 
-    },
-  },
-});
+})
