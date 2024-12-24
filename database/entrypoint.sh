03c51974065b0dfac46452ef5f6ecdd093d9916a
Ahmed Bakry <ahmedbakry024@gmail.com>
2024-12-24 13:00:55 +0200

    .

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -2,23 +2,24 @@
 set -e
 
 # Start MySQL with skip grant tables
-mysqld --skip-grant-tables &
+mysqld --skip-grant-tables --user=mysql &
 mysql_pid=$!
 
 # Wait for MySQL to start
-until mysqladmin ping; do
+until mysqladmin ping --host=127.0.0.1 --silent; do
+  echo "Waiting for MySQL to start..."
   sleep 1
 done
 
 # Reset the root password
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql --host=127.0.0.1 -e "FLUSH PRIVILEGES;"
+mysql --host=127.0.0.1 -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql --host=127.0.0.1 -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql --host=127.0.0.1 -e "FLUSH PRIVILEGES;"
 
 # Stop MySQL
 kill $mysql_pid
 wait $mysql_pid
 
 # Start MySQL normally
-exec mysqld
+exec mysqld --user=mysql
