d5ad77edcf3ff047bfefce3528b77ebd0c7214cc
Ahmed Bakry <ahmedbakry024@gmail.com>
2024-12-24 14:52:34 +0200

    frontend edits

--- frontend/src/services/apiService.jsx
+++ frontend/src/services/apiService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://golang-ahmedbakry-dev.apps.rm2.thpm.p1.openshiftapps.com";
 
 axios.defaults.baseURL = API_URL;
 axios.defaults.withCredentials = false; 
